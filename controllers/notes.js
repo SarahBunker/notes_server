8c9bca47e4b39a958e17d254f2a35c942e4e847b
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-25 12:02:25 -0700

    updating route for note so it also adds user id, two tests currently fail because not adding userid

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -15,18 +16,25 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
-notesRouter.post('/', async (request, response) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
     date: new Date(),
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
+
 
 notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndRemove(request.params.id)
